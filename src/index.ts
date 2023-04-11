4ae1af7eb176c1e634a1321f0b3adab84a25e4cc
hsiwe <46127681+Hsiwe@users.noreply.github.com>
2023-04-11 11:31:18 +0400

    Fix path in printTree(can be used in node_modules)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import fs from "fs/promises";
+import path from "path";
+
 import { generateTypeTree } from "./schema-printer/schema-printer";
 import type { UnitReflectionT } from "./schema-printer/schema-types";
+import { writeFileSync } from "fs";
 
 export async function printTree<T>(
   transformF: (units: T[]) => UnitReflectionT[],
   units: T[],
   treeName: string
 ): Promise<void> {
-  fs.writeFile(
-    `./generated/${treeName}.ts`,
+  const splitted = __dirname.split(path.sep);
+  splitted.pop();
+  splitted.push("generated");
+  const newPath = splitted.join(path.sep);
+
+  writeFileSync(
+    `${newPath}/${treeName}.ts`,
     generateTypeTree(transformF(units), treeName)
   );
 }
