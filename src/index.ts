8b655e46b8f4618dc3dff28c84785e5d5356834c
hsiwe <46127681+Hsiwe@users.noreply.github.com>
2023-06-17 12:30:03 +0300

    Move generated files to user's code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
-import path from 'path';
-
 import { generateTypeTree } from './schema-printer/schema-printer';
 import type { UnitReflectionT } from './schema-printer/schema-types';
-import { writeFileSync } from 'fs';
+import { writeFile, mkdir } from 'fs/promises';
 
 export async function printTree<T>(
   transformF: (units: T[]) => UnitReflectionT[],
   units: T[],
-  treeName: string
+  treeName: string,
+  dirPath: string
 ): Promise<void> {
-  const splitted = __dirname.split(path.sep);
-  splitted.pop();
-  splitted.push('generated');
-  const newPath = splitted.join(path.sep);
+  await mkdir(dirPath);
 
-  writeFileSync(
-    `${newPath}/${treeName}.ts`,
+  await writeFile(
+    `${dirPath}/${treeName}.ts`,
     generateTypeTree(transformF(units), treeName)
   );
 }
