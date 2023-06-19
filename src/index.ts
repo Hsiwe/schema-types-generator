5763d381a92f1a1b335f4fa043fa79c15ecbd995
hsiwe <46127681+Hsiwe@users.noreply.github.com>
2023-06-19 19:09:12 +0300

    Rewrite printTree using Task

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,30 @@ import { createTypeLiteralNode, createTypeTree } from './schema-printer/schema-p
 import type { UnitReflectionT } from './schema-printer/schema-types';
 import { writeFile, mkdir } from 'fs/promises';
 import { createSnapshot } from './schema-snapshots/create-snapshot';
+import * as T from 'fp-ts/lib/Task';
+import { pipe } from 'fp-ts/lib/function';
 
+const printTreeProgram = <T>(
+  transformF: (units: T[]) => UnitReflectionT[],
+  units: T[],
+  treeName: string,
+  dirPath: string
+): T.Task<void> =>
+  pipe(
+    T.Do,
+    T.flatMap(() => T.of(mkdir(dirPath))),
+    T.flatMap(() =>
+      T.of(writeFile(`${dirPath}/${treeName}.ts`, createTypeTree(transformF(units), treeName)))
+    ),
+    T.flatMap(() => T.of(undefined))
+  );
 export async function printTree<T>(
   transformF: (units: T[]) => UnitReflectionT[],
   units: T[],
   treeName: string,
   dirPath: string
 ): Promise<void> {
-  await mkdir(dirPath);
-
-  await writeFile(`${dirPath}/${treeName}.ts`, createTypeTree(transformF(units), treeName));
+  printTreeProgram(transformF, units, treeName, dirPath);
 }
 
 export async function printSnapshot<T>(
