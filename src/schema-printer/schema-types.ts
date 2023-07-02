c8476dd84915248f057ba2ea389f73781c341716
hsiwe <46127681+Hsiwe@users.noreply.github.com>
2023-07-02 10:40:45 +0300

    Remove io-ts UnitReflection

--- src/schema-printer/schema-types.ts
+++ src/schema-printer/schema-types.ts
@@ -1,34 +1,4 @@
-import * as t from 'io-ts';
 import ts from 'typescript';
-
-const UnitReflection: t.Type<UnitReflectionT> = t.recursion('UnitReflection', () =>
-  t.union([
-    t.type({
-      required: t.boolean,
-      key: t.string,
-      returnValue: t.union([
-        t.literal('string'),
-        t.literal('number'),
-        t.literal('boolean'),
-        t.literal('date'),
-        t.literal('select'),
-        t.literal('unknown'),
-      ]),
-    }),
-    t.type({
-      required: t.boolean,
-      key: t.string,
-      returnValue: t.literal('recursive'),
-      values: t.array(UnitReflection),
-    }),
-    t.type({
-      required: t.boolean,
-      key: t.string,
-      returnValue: t.literal('custom'),
-      type: t.any,
-    }),
-  ])
-);
 
 export type UnitReflectionReturnValue =
   | 'string'
